75cd6da875db2c8ddc2184eb04de6b6fd626f8c9
josmanmik <miljosmik@hotmail.com>
2016-07-06 19:42:58 +0200

    Version mejorada (II)
    Ahora mola el triple.

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 int
 main()
 {
-	printf("Hola mundo\n");
-	return -1;
+	SDL_Window *win;
+	//SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+
+	win = SDL_CreateWindow("Una ventana",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		800, 600,
+		0);
+
+	SDL_Delay(600);
+
+	SDL_DestroyWindow(win);
+	win = NULL;
+
+	SDL_Quit();
+
+	return 0;
 }
